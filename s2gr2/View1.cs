190ddfec1b82efdecdde766bd7d57fa8ad125218
psacharuk <piotr.sacharuk@gmail.com>
2013-05-24 10:01:40 +0200

    part3 - all the files

---
+++ s2gr2/View1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace s2gr2
+{
+    public partial class View1 : UserControl
+    {
+        public View1()
+        {
+            InitializeComponent();
+        }
+    }
+}
